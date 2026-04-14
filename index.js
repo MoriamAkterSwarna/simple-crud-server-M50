4751791280ed355220bc5153f9e3d778c25196ec
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-04-15 05:51:04 +0600

    code for Video-2 added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -22,6 +26,24 @@ app.get('/', (req, res) => {
 })
 
 
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+     // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
