49338cac50cab53c480c9a8b3efe9ee91cbd056f
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-04-15 17:01:21 +0600

    single data retrieve api creatd

--- index.js
+++ index.js
@@ -43,6 +43,15 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+     
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.findOne(query);
+        res.send(result);
+    });
+
+
     app.post('/users', async (req, res) => {
       const newUser = req.body;
    
